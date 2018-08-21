029f61b2d428766e1e6129a1ae2389ddd3001e69
Alano Acioli <alanoacioli@gmail.com>
2018-08-21 17:20:19 -0300

    - Added method to find a word in the file - Added method to get a list from file - Added method to get a random element

--- memory.py
+++ memory.py
@@ -1,3 +1,6 @@
+import random
+
+list = []
 
 #read file
 def read_file(path, action):
@@ -23,10 +26,45 @@ def menu():
             exit()
 
 def cad_word(fileName, language):
-    word = input("Digite the "+language+" word: ")
-    translation = input("Digite translation: ")
-    file = read_file(fileName, "a")
-    file.write(word+": "+translation+"\n")
-    file.close()
+    word = ""
+    while word != "0":
+        word = input("Digite the "+language+" word: ")
+        if not exist(fileName,word):
+            if(word != "0"):
+                translation = input("Digite translation: ")
+                file = read_file(fileName, "a")
+                file.write(word+" : "+translation+"\n")
+                file.close()
+        else:
+            print("Word already exist! Please try again")
 
-menu()
+def exist(fileName,word):
+    with open(fileName) as fp:
+        line = fp.readline()
+        while line:
+            if(word == line.strip().split(" ",1)[0]):
+                return True
+            line = fp.readline()
+        return False
+
+def get_list(fileName):
+
+    with open(fileName) as fp:
+        line = fp.readline()
+        while line:
+            list.append(line.strip())
+            line = fp.readline()
+        print(len(list))
+
+def get_random_element():
+    print(random.choice(list))
+
+
+
+#menu()
+
+#r = exist("en-pt.txt", "red:")
+#print(r)
+
+get_list("en-pt.txt")
+get_random_element()
